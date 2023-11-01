df96bbf11c2b34ad14004e9f6858fc63cafecc84
Johana-RHP <johana.rosicel@gmail.com>
2023-10-31 19:33:41 -0700

    fix

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,20 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node 3-starwars_title.js <film_id>');
-  process.exit(1);
-}
-
-const filmId = process.argv[2];
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${filmId}`;
-
-request(apiUrl, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const filmData = JSON.parse(body);
-    console.log(filmData.title);
-  } else {
-    console.error('Error: Unable to retrieve film title.');
+const id = process.argv[2];
+request('https://swapi-api.hbtn.io/api/films/' + id + '/', function (error, response, body) {
+  if (error == null) {
+    const json = JSON.parse(body);
+    console.log(json.title);
   }
 });
-
