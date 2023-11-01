eb74f90e38e95e13e937dbaa1838b9950208db8a
Johana-RHP <johana.rosicel@gmail.com>
2023-10-31 19:30:40 -0700

    fix

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,19 +1,3 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-if (process.argv.length !== 4) {
-  console.error('Usage: node 1-writeme.js <file_path> <content>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('File written successfully.');
-  }
-});
-
+fs.writeFileSync(process.argv[2], process.argv[3]);
